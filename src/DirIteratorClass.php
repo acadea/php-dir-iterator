137771fb6e0e6e055e362a39463bf51326b5dd65
Sam <opensource@acadea.io>
2021-06-17 10:14:35 +0800

    init project

---
+++ src/DirIteratorClass.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Acadea\DirIterator;
+
+class DirIteratorClass
+{
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Acadea\DirIterator\Tests;
 
 use PHPUnit\Framework\TestCase;
 
